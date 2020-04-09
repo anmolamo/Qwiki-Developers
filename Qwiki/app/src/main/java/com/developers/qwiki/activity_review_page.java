c1da953039319aa46ad4d8533ff0d21469fd40d6
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2020-04-09 17:18:18 +0530

    Review Page

---
+++ Qwiki/app/src/main/java/com/developers/qwiki/activity_review_page.java
@@ -0,0 +1,14 @@
+package com.developers.qwiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_review_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_page);
+    }
+}
