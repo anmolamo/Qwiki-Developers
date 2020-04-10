769dc14146e8ca8d71525274393146389e4cd993
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2020-04-10 22:36:32 +0530

    implementations

--- Qwiki/app/src/main/java/com/developers/qwiki/UserLogin.java
+++ Qwiki/app/src/main/java/com/developers/qwiki/UserLogin.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.developers.qwiki.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
