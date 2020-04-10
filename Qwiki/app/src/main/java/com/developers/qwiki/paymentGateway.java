b6ad498825f6870f9d2af22a29e5f01cb4826945
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2020-04-10 14:13:49 +0530

    Payement Gateway And Payment Verification, Summary Front end Part...

---
+++ Qwiki/app/src/main/java/com/developers/qwiki/paymentGateway.java
@@ -0,0 +1,14 @@
+package com.developers.qwiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class paymentGateway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_gateway);
+    }
+}
