a5d3fa9df56733862b0a66a604fa8beda099099f
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2020-04-12 00:55:07 +0530

    helper Signup

---
+++ Qwiki/app/src/main/java/com/developers/qwiki/HelperDocVerify.java
@@ -0,0 +1,14 @@
+package com.developers.qwiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelperDocVerify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helper_doc_verify);
+    }
+}
