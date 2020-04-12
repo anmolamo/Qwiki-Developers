69845e5638c15514e3e75ef0e230e54afba0f074
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2020-04-13 02:06:20 +0530

    Main Pages For Helpers

---
+++ Qwiki/app/src/main/java/com/developers/qwiki/doctorMain.java
@@ -0,0 +1,14 @@
+package com.developers.qwiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doctorMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor_main);
+    }
+}
---
+++ Qwiki/app/src/main/java/com/developers/qwiki/groceryMain.java
@@ -0,0 +1,14 @@
+package com.developers.qwiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class groceryMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grocery_main);
+    }
+}
